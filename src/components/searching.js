7adda267c834b06f1b3a7f47893b0cf9bafa3111
Yulia Deltsova <juliedeltsova@yandex.ru>
2026-03-29 01:11:31 +0300

    Table modified

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,11 +1,26 @@
-import {rules, createComparison} from "../lib/compare.js";
-
+import { rules, createComparison } from "../lib/compare.js";
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+  const searchComparator = createComparison(
+    ["skipEmptyTargetValues"],
+    [
+      rules.searchMultipleFields(
+        searchField,
+        ["date", "customer", "seller"],
+        false,
+      ),
+    ],
+  );
 
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data;
+  return (data, state, action) => {
+    const searchValue = state[searchField];
+    if (!searchValue || searchValue.trim() === "") {
+      return data;
     }
+
+    return data.filter((item) => {
+      const target = { [searchField]: searchValue };
+      return searchComparator(item, target);
+    });
+  };
 }
